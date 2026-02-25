45f4a97a7193e82e900315127b49496cab7b54c4
mirco <mirco.caneschi@stud.unifi.it>
2026-02-25 12:46:27 +0100

    old db free trial expired. fix with no consistency db

--- event_menagement_project/settings.py
+++ event_menagement_project/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', 'fallback-insecure-secret-key')
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['.onrender.com']
+ALLOWED_HOSTS = ['event-menagement-project.onrender.com', 'localhost', '127.0.0.1']
 
 # Application definition
 
